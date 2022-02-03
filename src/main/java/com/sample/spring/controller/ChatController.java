140652fbbf0ab703c75ac4448c1cf7a01acf778a
khokhoa <dbspgnl@naver.com>
2022-02-03 23:11:54 +0900

    + 채팅방 초기 설정

--- src/main/java/com/sample/spring/controller/ChatController.java
+++ src/main/java/com/sample/spring/controller/ChatController.java
@@ -1,25 +1,19 @@
 package com.sample.spring.controller;
 
-// import com.sample.spring.model.Board;
-// import com.sample.spring.repository.BoardRepository;
-// import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/board")
-public class BoardController {
-
-    // @Autowired
-    // private BoardRepository boardRepository;
+@RequestMapping("/chat")
+public class ChatController {
 
     @GetMapping("/list")
     public String list(Model model) {
         // List<Board> boards = boardRepository.findAll();
         // model.addAttribute("boards", boards);
-        return "contents/board/list";
+        return "contents/chat/list";
     }
-
+    
 }
--- src/main/java/com/sample/spring/controller/HomeController.java
+++ src/main/java/com/sample/spring/controller/HomeController.java
@@ -8,7 +8,7 @@ public class HomeController {
 
     @GetMapping
     public String index() {
-        return "main/index";
+        return "contents/main/index";
     }
 
 }
