4af698ecb594337d40116edfd844ba351ae3a7e2
DESKTOP-1BI7C0V\user <swpark@yellotail.com>
2022-02-11 17:55:02 +0900

    + 채팅방 리스트에서 제목 클릭 시 Room으로 이동하는 기능 일부 구현

--- src/main/java/com/sample/spring/controller/ChatController.java
+++ src/main/java/com/sample/spring/controller/ChatController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -23,6 +24,13 @@ public class ChatController {
         List<Room> rooms = roomRepository.findAll();
         model.addAttribute("rooms", rooms);
         return "contents/chat/list";
+    }
+
+	@GetMapping(value = "/{roomId}")
+	public String enterRoom(
+		@PathVariable("roomId") String roomId 
+	){
+        return "contents/chat/room";
     }
     
 }
